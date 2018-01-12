a2c3b8727b8c9143328f1b1cc8c4aa1b5d90f50f
Yihan Lin <yihanlin@usc.edu>
2018-01-12 13:16:43 -0800

    added two sentences

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,4 +4,6 @@ int main()
 {
   std::cout << "HelloWorld!" << std::endl;
   std::cout << "CS104" << std::endl;
+  std::cout << "I so hungry" << std::endl;
+  std::cout << "what should I do" << std::endl;
 }
