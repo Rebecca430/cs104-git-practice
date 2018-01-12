d55664075cd5cbe952107d7da14549c48e128fb3
Yihan Lin <yihanlin@usc.edu>
2018-01-12 13:18:52 -0800

    delete one line

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,5 +5,4 @@ int main()
   std::cout << "HelloWorld!" << std::endl;
   std::cout << "CS104" << std::endl;
   std::cout << "I so hungry" << std::endl;
-  std::cout << "what should I do" << std::endl;
 }
